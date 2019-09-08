7159b7e27fe56352b513b696695a8ee6aecf846b
Arjun Chauhan <arjun99.chauhan@gmail.com>
2019-09-08 18:14:10 +0530

    worked on nav links & css. Added redux & reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
 
-
+const store = createStore(rootReducer);
 ReactDOM.render(<App />, document.getElementById('app'));
